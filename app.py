c3ac9a3631b0aa67fc7ce815f2a70c0aeb07be47
Adhvaith <adhvaith.thurvas@gmail.com>
2024-09-10 16:21:57 -0700

    Polls update 12.0

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def serve_layout():
             #     interval=1*1000, # every second, for debug purposes
             #     n_intervals=0
             # ),
-            html.H4(children=f'Last updated: September 10, 2024 7:03 PM UTC', style={'textAlign':'center', 'font-family':'Lucida Console'}, id='last-updated'),
+            html.H4(children=f'Last updated: September 10, 2024 11:21 PM UTC', style={'textAlign':'center', 'font-family':'Lucida Console'}, id='last-updated'),
             # html.H4(children=f'Debug: {str(datetime.datetime.now())}', style={'textAlign':'center', 'font-family':'Lucida Console'}, id='debug-last-updated'),
             html.Hr(),
             html.H2(children='Overview', style={'textAlign':'center', 'font-family':'Lucida Console'}),
