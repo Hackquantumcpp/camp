d9dc328c5a66575da177a604ea5bd54befb4d5e3
Adhvaith <adhvaith.thurvas@gmail.com>
2024-10-11 01:21:40 -0700

    Re-ran fundamentals model

--- fundamentals_model_output.py
+++ fundamentals_model_output.py
@@ -1,5 +1,5 @@
-pred_harris_stdev = 33.32702312156544
-pred_trump_stdev = 20.06970290485785
-pred_harris_val = 48.3950305213298
-pred_trump_val = 46.90642576787326
-pred_margin = 1.488604753456542
+pred_harris_stdev = 32.588719654881984
+pred_trump_stdev = 19.047798094629677
+pred_harris_val = 48.59549003548907
+pred_trump_val = 46.94897714054615
+pred_margin = 1.6465128949429229
